e48ec035c4b77db9cb2d02287bbc5fcced9a86f5
Lodestar0712 <102283055+Lodestar0712@users.noreply.github.com>
2022-06-09 07:58:35 +0300

    Add files via upload

---
+++ newapp/News/newapp/views.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from django.views.generic import ListView, DetailView
+from .models import *
+
+
+class PostList(ListView):
+    model = Post
+    ordering = '-dateCreation'
+    template_name = 'News.html'
+    context_object_name = 'News'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        return context
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'New.html'
+    context_object_name = 'New'
